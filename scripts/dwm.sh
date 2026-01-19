4b7142f522024cff7b512d56d4ed014521569088
Steven de jong 🍟 <sdejong@cloudsuite.com>
2026-01-19 08:53:10 +0100

    add packages

--- scripts/dwm.sh
+++ scripts/dwm.sh
@@ -1,9 +1,19 @@
 #! /usr/bin/env bash
 
 download_repo() {
+  if [ "$(id -u)" -eq 0 ]; then
+    HOME_DIR=$(eval echo "~$SUDO_USER")
+  else
+    HOME_DIR="$HOME"
+  fi
+
+  if [ ! -d "$HOME_DIR/builds" ]; then
+    mkdir -p "$HOME_DIR/builds"
+  fi
+
   repo_url="git@github.com:stevendejongnl/suckless.git"
-  target_dir="$HOME/suckless"
-  
+  target_dir="$HOME_DIR/builds/suckless"
+
   if [ -d "$target_dir" ]; then
     echo "Repository already exists at $target_dir. Pulling latest changes..."
     cd "$target_dir" || exit
@@ -26,4 +36,4 @@ install_slstatus() {
 
 download_repo
 install_dwm
-install_slstatus
+# install_slstatus
